9fdf168635e27c9d4588daa00f8a609bef2ce89a
knd <khanhdao.knd@gmail.com>
2019-08-22 16:15:51 +0700

    Implement listing operations in repository

--- pkg/storage/memory/repository.go
+++ pkg/storage/memory/repository.go
@@ -1,8 +1,15 @@
 package memory
 
 import (
+	"errors"
+	"log"
+
+	"github.com/knd/kndchain/pkg/listing"
 	"github.com/knd/kndchain/pkg/mining"
 )
+
+// ErrAddNilBlock is used when no mined block is given to add
+var ErrAddNilBlock = errors.New("Mined block is not given to add")
 
 // MemStorage keeps blockchain in memory
 type MemStorage struct {
@@ -18,7 +25,9 @@ func NewRepository() *MemStorage {
 
 // AddBlock adds mined block into blockchain
 func (m *MemStorage) AddBlock(minedBlock *mining.Block) error {
-	// TODO: validations of minedBlock
+	if minedBlock == nil {
+		return ErrAddNilBlock
+	}
 
 	newB := Block{
 		Timestamp: minedBlock.Timestamp,
@@ -30,4 +39,25 @@ func (m *MemStorage) AddBlock(minedBlock *mining.Block) error {
 	m.blockchain.chain = append(m.blockchain.chain, newB)
 
 	return nil
+}
+
+// GetBlockCount returns the latest block count in blockchain
+func (m *MemStorage) GetBlockCount() uint32 {
+	return uint32(len(m.blockchain.chain))
+}
+
+// GetLastBlock returns the last block in blockchain
+func (m *MemStorage) GetLastBlock() listing.Block {
+	if m.GetBlockCount() == 0 {
+		log.Fatal("Blockchain is empty")
+	}
+
+	lastBlock := m.blockchain.chain[m.GetBlockCount()-1]
+
+	return listing.Block{
+		Timestamp: lastBlock.Timestamp,
+		LastHash:  lastBlock.LastHash,
+		Hash:      lastBlock.Hash,
+		Data:      lastBlock.Data,
+	}
 }
