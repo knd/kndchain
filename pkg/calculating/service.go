db824fef46faef961227a50be7108fc34223fdb1
knd <khanhdao.knd@gmail.com>
2019-09-10 10:12:51 +0700

    Add log to calculating service

--- pkg/calculating/service.go
+++ pkg/calculating/service.go
@@ -1,13 +1,15 @@
 package calculating
 
 import (
+	"log"
+
 	"github.com/knd/kndchain/pkg/config"
 )
 
 // Service provides access to calculating operations
 type Service interface {
 	Balance(address string, bc *Blockchain) uint64
-	BalanceByBlockIndex(address string, bc *Blockchain, i int) uint64
+	BalanceByBlockIndex(address string, bc *Blockchain, index int) uint64
 }
 
 type service struct{}
@@ -25,12 +27,15 @@ func (s *service) Balance(address string, bc *Blockchain) uint64 {
 func (s *service) BalanceByBlockIndex(address string, bc *Blockchain, index int) uint64 {
 	var balance uint64
 	if bc == nil || len(bc.Chain) == 0 {
+		log.Println("BalanceByBlockIndex: blockchain is nil or chain length is 0")
 		return config.InitialBalance
 	}
 	if index >= len(bc.Chain) {
+		log.Printf("BalanceByBlockIndex: Index=%d is greater than chain length=%d, setting index to chain length", index, len(bc.Chain))
 		index = len(bc.Chain) - 1
 	}
 	if index < 0 {
+		log.Printf("BalanceByBlockIndex: Index=%d is less than 0, setting index to 0", index)
 		index = 0
 	}
 
